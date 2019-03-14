bd97d3db7c072cbd220dea5ff0425a64734acc38
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2019-03-15 00:43:03 +0100

    Added meta programming utilities.

--- include/structocol/type_utilities.hpp
+++ include/structocol/type_utilities.hpp
@@ -11,6 +11,32 @@
 
 namespace structocol {
 
+template <std::size_t index, typename T, typename... Args>
+struct nth_type {
+	using type = typename nth_type<index - 1, Args...>::type;
+};
+
+template <typename T, typename... Args>
+struct nth_type<0, T, Args...> {
+	using type = T;
+};
+
+template <typename T, typename U, typename... Args>
+struct index_of_type {
+	static constexpr std::size_t value = index_of_type_impl<T, Args...>::value + 1;
+};
+
+template <typename T, typename U>
+struct index_of_type<T, U> {};
+
+template <typename T, typename... Args>
+struct index_of_type<T, T, Args...> {
+	static constexpr std::size_t value = 0;
+};
+
+template <typename T, typename Types>
+constexpr std::size_t index_of_type_v = index_of_type<T, Types...>::value;
+
 } // namespace structocol
 
 #endif // STRUCTOCOL_TYPE_UTILITIES_INCLUDED
