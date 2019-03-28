80872a1135a139e81ece4a5447e08d1f8c9361a3
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2019-03-28 18:23:35 +0100

    Revert "Revert "Removed dependency on boost::asio::dynamic_buffer.""
    This reverts commit 94ca71e69c3098866ec1453005b0e92840f243f4.

--- include/structocol/vector_buffer.hpp
+++ include/structocol/vector_buffer.hpp
@@ -165,10 +165,8 @@ public:
 			: vb_{vb}, max_size_{vb.raw_vector_.max_size()} {}
 	explicit vector_buffer_dynamic_view(vector_buffer<Trim_Policy>& vb, std::size_t max_size)
 			: vb_{vb}, max_size_{max_size} {}
-	using const_buffers_type = decltype(
-			boost::asio::dynamic_buffer(std::declval<std::vector<std::byte>&>()))::const_buffers_type;
-	using mutable_buffers_type = decltype(
-			boost::asio::dynamic_buffer(std::declval<std::vector<std::byte>&>()))::mutable_buffers_type;
+	using const_buffers_type = boost::asio::BOOST_ASIO_CONST_BUFFER;
+	using mutable_buffers_type = boost::asio::BOOST_ASIO_MUTABLE_BUFFER;
 
 	std::size_t size() const {
 		return vb_.available_bytes();
