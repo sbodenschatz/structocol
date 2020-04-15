74cc514215b8404a16508afdbf81f7d69e34668c
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2020-04-15 17:01:43 +0200

    Added header stub for exceptions.

---
+++ include/structocol/exceptions.hpp
@@ -0,0 +1,14 @@
+/*
+ * Structocol project
+ *
+ * Copyright 2020
+ */
+
+#ifndef STRUCTOCOL_EXCEPTIONS_INCLUDED
+#define STRUCTOCOL_EXCEPTIONS_INCLUDED
+
+#include <stdexcept>
+
+namespace structocol {} // namespace structocol
+
+#endif // STRUCTOCOL_EXCEPTIONS_INCLUDED
