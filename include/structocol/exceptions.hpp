c777dbb80f8e28f2a2f9038197f682877d74e9f7
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2020-04-15 18:40:10 +0200

    Added exception base classes structocol::length_error and structocol::runtime_error to categorize exceptions.

--- include/structocol/exceptions.hpp
+++ include/structocol/exceptions.hpp
@@ -11,24 +11,30 @@
 
 namespace structocol {
 
-class message_length_overflow : public std::length_error {
+class length_error : public std::length_error {
 public:
 	using std::length_error::length_error;
 };
 
-class buffer_length_error : public std::length_error {
-public:
-	using std::length_error::length_error;
+class message_length_overflow : public length_error {
+	using length_error::length_error;
 };
 
-class io_error : public std::runtime_error {
+class buffer_length_error : public length_error {
+	using length_error::length_error;
+};
+
+class runtime_error : public std::runtime_error {
 public:
 	using std::runtime_error::runtime_error;
 };
 
-class deserialization_data_error : public std::runtime_error {
-public:
-	using std::runtime_error::runtime_error;
+class io_error : public runtime_error {
+	using runtime_error::runtime_error;
+};
+
+class deserialization_data_error : public runtime_error {
+	using runtime_error::runtime_error;
 };
 
 } // namespace structocol
